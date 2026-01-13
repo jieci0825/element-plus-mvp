337e0ca0b864b1c156c7262979259417fa0e147b
coderjc <coderjc@qq.com>
2026-01-14 01:29:46 +0800

    refactor: 优化 ep-alias 插件的路径处理逻辑，更新外部依赖配置

--- build/plugins/ep-alias.ts
+++ build/plugins/ep-alias.ts
@@ -12,12 +12,14 @@ export function EpAlias(): Plugin {
         name: 'ep-alias-plugin',
         resolveId(id) {
             // 如果源主题样式路径不是以 @ep/theme-chalk 开头，则不处理
-            if (!id.startsWith(sourceThemeChalk)) return
+            if (!id.startsWith(sourceThemeChalk)) {
+                return
+            }
             return {
                 // 如果源主题样式路径是以 @ep/theme-chalk 开头，则替换为 ep/theme-chalk，即更换掉导入的路径字符串
                 id: id.replaceAll(sourceThemeChalk, bundleThemeChalk),
                 // 将主题样式路径设置为外部依赖，即让 rollup 打包的时候，认为是一个外部依赖，直接忽略掉，不会打包到 bundle 中
-                external: true
+                external: 'absolute'
             }
         }
     }
